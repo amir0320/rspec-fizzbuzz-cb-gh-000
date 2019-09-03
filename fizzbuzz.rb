bfc67da829ae759159d2ce4fac484a01429f12a4
Yijing Nie <hfdy0320@gmail.com>
2019-09-03 03:16:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-
+        if num % 15 == 0
+                return "FizzBuzz"
+        elif num % 3 == 0
+                return "Fizz"
+        elif num % 5 == 0
+                return "Buzz"
+        else
+                return nil
 end
